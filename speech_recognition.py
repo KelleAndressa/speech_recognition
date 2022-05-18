953aedc4711853fecac7f5cd73ccf377f0488edb
Kelle Andressa <101825267+KelleAndressa@users.noreply.github.com>
2022-05-18 20:12:34 +0100

    ...

---
+++ speech_recognition.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+
+rec = sr.Recognizer()
+
+mic = sr.Microphone()
+#print(sr.Microphone().list_microphone_names())
+
+with sr.Microphone() as mic:
+    rec.adjust_for_ambient_noise(mic)
+    print("Pode falar que eu vou gravar")
+    audio = rec.listen(mic)
+    texto = rec.recognize_google(audio, language="pt-BR")
+    print(texto)
+
+
+
+
+# Creditos Teacher Lira.. Hashtag Programação
